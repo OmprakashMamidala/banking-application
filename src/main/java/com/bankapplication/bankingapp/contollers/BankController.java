d61a371684a1ee1ede61338b1d6207c7a06b7c35
omprakash <mamidalaompraksh3@gmail.com>
2024-05-28 11:46:14 +0530

    changes made in the uri's as per rest api's

--- src/main/java/com/bankapplication/bankingapp/contollers/BankController.java
+++ src/main/java/com/bankapplication/bankingapp/contollers/BankController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,38 +12,41 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bankapplication.bankingapp.dto.AccountDto;
 import com.bankapplication.bankingapp.service.AccountService;
 
-@RestController()
-@RequestMapping("/api/bank")
+@RestController
+@RequestMapping("/api/accounts")
+
 public class BankController {
 	@Autowired
 	private AccountService accountService;
 
-	@PostMapping("/addAccount")
+	@PostMapping
 	public AccountDto saveAccount(@RequestBody AccountDto accountDto) {
 		return accountService.addAccount(accountDto);
 	}
 
 	@GetMapping("/{account_id}")
 	public AccountDto getAccountById(@PathVariable("account_id") Integer id) {
+		
 		return accountService.getAccountById(id);
 	}
 
-	@PutMapping("/{account_id}")
+	@PutMapping("/{account_id}/deposit")
 	public AccountDto deposit(@PathVariable("account_id") Integer id, Map<String,Double>map) {
 		return accountService.deposit(id, map.get("amount"));
 	}
 
-	@PutMapping("withdraw/{account_id}")
+	@PutMapping("/{account_id}/withdraw")
 	public AccountDto withDrawBalance(@PathVariable("account_id") Integer id, @RequestBody Map<String,Double>map) {
 		return accountService.withDraw(id, map.get("amount"));
 	}
 
-	@GetMapping("accounts")
+	@GetMapping
 	public List<AccountDto> getAllAccounts() {
 		return accountService.allAccounts();
 	}
